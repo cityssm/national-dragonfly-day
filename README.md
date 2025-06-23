403f93b92386e656fffb6546ca26b3a36479effd
Dan Gowans <dangowans@users.noreply.github.com>
2025-06-23 13:54:15 -0400

    defend clergue park!

--- README.md
+++ README.md
@@ -1,46 +1,19 @@
-[Play the game](https://farvic.github.io/mosquito-game/)
+# National Dragonfly Day - Defend Clergue Park
 
-# Introduction
+![Screenshot](images/screenshot.png)
 
-Kill the mosquito game developed by Victor Araujo during the Javascript course.
+🕹️ [Play the Game](https://cityssm.github.io/national-dragonfly-day)
 
-Since the game was initially developed during an early stage of the course, a leaderboard was not added to the game.
+## How to Play
 
-## Features
+Use the dragonfly to eat the mosquitoes!
 
-The game allows the user to choose a level which will affect on how long the mosquitoes will stay on the screen.
+Click the mosquitoes with the dragonfly's mouth to make them disappear.
 
-## What could be improved
+## Credits
 
-* A leaderboard could be added to game.
-* Change the way how the parameters are sent to the next page, to avoid passing them through the href.
+Game adapted from [mosquito-game](https://github.com/farvic/mosquito-game).
 
-Any feedback is welcome.
-
-## Updates
-
-## Patch 1.0.5
-
-### The user is now able to set the game duration to 30, 60 or 90 seconds.
-
-## Patch 1.0.4
-
-### Difficulty was decreased and now the user earns more points based on the level.
-
-* Easy:
-    * Mosquito life: 1500ms
-* Hard:
-    * Mosquito life: 750ms
-    * Now it gives 2 points/kill.
-* OMG:
-    * Mosquito life: changed from 500ms to 600ms.
-    * Now it gives 3 points/kill. 
-
-
-## Patch 1.0.3
-
-### Difficulty was increased.
-
-* Easy: nothing changed (1500ms)
-* Hard: mosquito life-cycle changed from 1000ms -> to 750ms
-* OMG: mosquito life-cycle changed from 750ms -> to 500ms
+- [Bootstrap](https://getbootstrap.com/)
+- [Animate,css](https://animate.style/)
+- [Dragonfly cursor](https://pixabay.com/illustrations/dragonfly-damselfly-insect-cartoon-9101902/)
--- logic.js
+++ logic.js
@@ -1,137 +1,122 @@
-var width = 0;
-var height = 0;
-var lives = 3;
-var time = 60;
-var score = 0;
-var multiply = 1;
+let lives = 3;
+let score = 0;
 
-var mosquitoLife = 1500;
-var level = '';
-var search = window.location.search;
-search = search.replace('?','');
-var split = search.split('&',2);
-level = split[0];
-time = split[1];
+const mosquitoLife = 1500;
+const multiply = 1;
 
-console.log(level);
-// console.log(time);
+/*
+ * Game screen size
+ */
 
-// document.getElementById('final-score').innerHTML = finalScore;
-
-//Game difficulty
-
-if(level==='easy') {
-    mosquitoLife = 1500;
-    multiply = 1;
-}
-if(level==='hard') {
-    mosquitoLife = 750;
-    multiply = 2;
-}
-else if(level==='omg') {
-    mosquitoLife = 600;
-    multiply = 3;
-}
-
-// Game duration
-
-if(time === 'thirty') time = 30;
-else if (time === 'sixty') time = 60;
-else time = 90;
-
-// var score = document.getElementById('score');
+let margin = 100;
+let width = 0;
+let height = 0;
 
 function adjustGameScreenSize() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    console.log(width, height);
+  width = window.innerWidth;
+  height = window.innerHeight;
 }
-
-var chronometer = setInterval(function(){
-    time--;
-    if (time < 0) {
-        clearInterval(chronometer);
-        clearInterval(createMosquito);
-        window.location.href="victory.html?"+score+'('+level+')';
-    } else document.getElementById('chronometer').innerHTML = Math.floor(time);
-},mosquitoLife);
 
 adjustGameScreenSize();
 
+/*
+ * Mosquito Logic
+ */
 
 function randomMosquitoPosition() {
-    // Remove the previous mosquito (if there's any...)
-    if (document.getElementById('mosquito')) {
-        document.getElementById('mosquito').remove();
+  // Remove the previous mosquito (if there's any...)
+  if (document.getElementById("mosquito")) {
+    document.getElementById("mosquito").remove();
 
-        if(lives > 0) {
-            document.getElementById('life'+lives).src="images/empty_heart.png";
-            lives--;
-        } else{
-            window.location.href="game_over.html?"+score+'('+level+')';
-        };
-        if(lives == 0) window.location.href="game_over.html?"+score+'('+level+')';
-    }
-    //background-size 1280x1017
-    //! Needs to add mobile resposiveness
-    var maxWidth = (width/2) + 640;
-    var minWidth = (width/2) - 640;
-    // var maxHeight = (height/2) + 508.5;
-    // var minHeight = (height/2) - 508.5;
-    var positionX = 0;
-    if (width >= 1024) {
-        positionX = (Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth - 90);
-    } else {
-        positionX = Math.floor(Math.random() * width) - 90;
-        minWidth = 0;
+    if (lives > 0) {
+      document.getElementById("life" + lives).src = "images/empty_heart.png";
+      lives--;
     }
 
-    // var positionY = Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight - 90;
-
-    // var positionX = Math.floor(Math.random() * width) - 90;
-    var positionY = Math.floor(Math.random() * height) - 90;
-
-    positionX = positionX < minWidth ? minWidth : positionX;
-    positionY = positionY < 0 ? 0 : positionY;
-    
-    // Create the html element
-    var mosquito = document.createElement('img');
-    mosquito.src = 'images/mosquito.png';
-    mosquito.className = randomMosquitoSize() + ' ' + randomMosquitoDirection();
-    mosquito.style.left = positionX + 'px';
-    mosquito.style.top = positionY + 'px';
-    mosquito.style.position = 'absolute';
-    mosquito.id = 'mosquito';
-    mosquito.onclick = function () {
-        this.remove();
-        score = score + multiply;
-        // time+=0.2;
-        document.getElementById('current-score').innerHTML = score;
+    if (lives <= 0) {
+      window.location.href = "gameOver.html?" + score;
     }
+  }
+  //background-size 1280x1017
+  //! Needs to add mobile resposiveness
+  var maxWidth = width / 2 + 640;
+  var minWidth = width / 2 - 640;
 
-    document.body.appendChild(mosquito);
+  var positionX = 0;
+  if (width >= 1024) {
+    positionX =
+      Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth - margin;
+  } else {
+    positionX = Math.floor(Math.random() * width) - margin;
+    minWidth = 0;
+  }
+
+  var positionY = Math.floor(Math.random() * height) - margin;
+
+  positionX = Math.max(positionX < minWidth ? minWidth : positionX, margin);
+  positionY = Math.max(positionY < 0 ? 0 : positionY, margin);
+
+  // Create the html element
+  const mosquito = document.createElement("img");
+  mosquito.src = "images/mosquito.png";
+  mosquito.className = randomMosquitoSize() + " " + randomMosquitoDirection();
+  mosquito.style.left = positionX + "px";
+  mosquito.style.top = positionY + "px";
+  mosquito.style.position = "absolute";
+  mosquito.id = "mosquito";
+  mosquito.onclick = function () {
+    this.remove();
+    score += multiply;
+    document.getElementById("current-score").innerHTML = score;
+  };
+
+  document.body.appendChild(mosquito);
 }
 
 function randomMosquitoSize() {
-    var mosquitoClass = Math.floor(Math.random() * 3);
+  const mosquitoSizeClass = Math.floor(Math.random() * 3);
 
-    switch(mosquitoClass) {
-        case 0:
-            return 'mosquito1';
-        case 1:
-            return 'mosquito2';
-        case 2:
-            return 'mosquito3';
-    }
+  switch (mosquitoSizeClass) {
+    case 0:
+      return "mosquito1";
+    case 1:
+      return "mosquito2";
+    case 2:
+      return "mosquito3";
+  }
 }
 
 function randomMosquitoDirection() {
-    var mosquitoClass = Math.floor(Math.random() * 2);
+  const mosquitoDirectionClass = Math.floor(Math.random() * 2);
 
-    switch(mosquitoClass) {
-        case 0:
-            return 'direction_left';
-        case 1:
-            return 'direction_right';
-    }
+  switch (mosquitoDirectionClass) {
+    case 0:
+      return "direction_left";
+    case 1:
+      return "direction_right";
+  }
 }
+
+const createMosquitoInterval = setInterval(function () {
+  randomMosquitoPosition();
+}, mosquitoLife);
+
+/*
+ * Timer
+ */
+
+let time = 60;
+
+const chronometerElement = document.getElementById("chronometer");
+chronometerElement.textContent = Math.floor(time);
+
+const chronometerInterval = setInterval(function () {
+  time--;
+  if (time < 0) {
+    clearInterval(chronometerInterval);
+    clearInterval(createMosquitoInterval);
+    window.location.href = "gameOver.html?" + score;
+  } else {
+    document.getElementById("chronometer").innerHTML = Math.floor(time);
+  }
+}, 1000);
