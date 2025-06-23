c072e847bc87ff2669bfe4aebfce1839ee73628b
Dan Gowans <dangowans@users.noreply.github.com>
2025-06-23 15:10:52 -0400

    linting

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 🕹️ [Play the Game](https://cityssm.github.io/national-dragonfly-day)
 
+A simple "swat the mosquito" game for the 2025 National Dragonfly Day door decorating contest.
+
 ## How to Play
 
 Use the dragonfly to eat the mosquitoes!
@@ -17,3 +19,11 @@ Game adapted from [mosquito-game](https://github.com/farvic/mosquito-game).
 - [Bootstrap](https://getbootstrap.com/)
 - [Animate,css](https://animate.style/)
 - [Dragonfly cursor](https://pixabay.com/illustrations/dragonfly-damselfly-insect-cartoon-9101902/)
+
+## Past Door Competition Entries
+
+[National Bee Day](https://github.com/cityssm/national-bee-day)<br />
+A "spot the queen bee" game for the 2024 National Bee Day door competiton.
+
+[Canada Day Fireworks](https://github.com/cityssm/canada-day-fireworks)<br />
+A fireworks show for the 2023 Canada Day door competition!
---
+++ js/logic.ts
@@ -0,0 +1,133 @@
+// eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
+/* eslint-disable sonarjs/pseudo-random, unicorn/prefer-global-this */
+;(() => {
+  let lives = 3
+  let score = 0
+
+  const mosquitoLife = 1500
+  const multiply = 1
+
+  /*
+   * Game screen size
+   */
+
+  const margin = 100
+  let width = 0
+  let height = 0
+
+  function adjustGameScreenSize(): void {
+    width = window.innerWidth
+    height = window.innerHeight
+  }
+
+  adjustGameScreenSize()
+
+  /*
+   * Mosquito Logic
+   */
+
+  function randomMosquitoPosition(): void {
+    // Remove the previous mosquito (if there's any...)
+    if (document.querySelector('#mosquito') !== null) {
+      document.querySelector('#mosquito')?.remove()
+
+      if (lives > 0) {
+        document.getElementById('life' + lives).src = 'images/empty_heart.png'
+        lives--
+      }
+
+      if (lives <= 0) {
+        window.location.href = 'gameOver.html?' + score
+      }
+    }
+
+    const maxWidth = width / 2 + 640
+    let minWidth = width / 2 - 640
+
+    let positionX = 0
+    if (width >= 1024) {
+      positionX =
+        Math.floor(Math.random() * (maxWidth - minWidth + 1)) +
+        minWidth -
+        margin
+    } else {
+      positionX = Math.floor(Math.random() * width) - margin
+      minWidth = 0
+    }
+
+    let positionY = Math.floor(Math.random() * height) - margin
+
+    positionX = Math.max(positionX, minWidth, margin)
+    positionY = Math.max(positionY, margin)
+
+    // Create the html element
+    const mosquito = document.createElement('img')
+    mosquito.src = 'images/mosquito.png'
+    mosquito.className = randomMosquitoSize() + ' ' + randomMosquitoDirection()
+    mosquito.style.left = positionX + 'px'
+    mosquito.style.top = positionY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    mosquito.addEventListener('click', function () {
+      this.remove()
+      score += multiply
+      document.querySelector('#current-score').innerHTML = score
+    })
+
+    document.body.appendChild(mosquito)
+  }
+
+  function randomMosquitoSize(): string {
+    const mosquitoSizeClass = Math.floor(Math.random() * 3)
+
+    switch (mosquitoSizeClass) {
+      case 0: {
+        return 'mosquito1'
+      }
+      case 1: {
+        return 'mosquito2'
+      }
+      default: {
+        return 'mosquito3'
+      }
+    }
+  }
+
+  function randomMosquitoDirection(): string {
+    const mosquitoDirectionClass = Math.floor(Math.random() * 2)
+
+    return mosquitoDirectionClass === 0
+      ? 'direction_left'
+      : 'direction_right'
+  }
+
+  const createMosquitoInterval = setInterval(function () {
+    randomMosquitoPosition()
+  }, mosquitoLife)
+
+  /*
+   * Timer
+   */
+
+  let time = 60
+
+  const chronometerElement = document.querySelector('#chronometer')
+
+  if (chronometerElement === null) {
+    throw new Error('Chronometer element not found')
+  }
+
+  chronometerElement.textContent = Math.floor(time).toString()
+
+  const chronometerInterval = setInterval(function () {
+    time--
+    if (time < 0) {
+      clearInterval(chronometerInterval)
+      clearInterval(createMosquitoInterval)
+      window.location.href = 'gameOver.html?' + score
+    } else {
+      chronometerElement.textContent = Math.floor(time).toString()
+    }
+  }, 1000)
+})()
